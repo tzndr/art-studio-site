24b6609dd4f56f257424af3d5cc55571b9dae4ae
Timothy Zander <connect@timzander.com>
2019-05-22 11:20:01 -0500

    Add all import scripts to html template files

--- static/js/app.js
+++ static/js/app.js
@@ -1,18 +1,8 @@
-//Declaring global variables
-
 
 var ViewModel = function() {
   var self = this;
 
-  var currentImage = 0;
-
-  var imageElement = document.getElementById('image-changer');
-
-  function nextImage(){
-      currentImage = (currentImage + 1) % mainImages.length;
-      imageElement.src = mainImages[currentImage];
-  }
-
-  var timeoutId = setTimeout(nextImage, 1000);
 
 }
+
+ko.applyBindings(new ViewModel());
