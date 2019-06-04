5d6f0847d3c0ab9655942846567a654c7f63fb4a
Timothy Zander <connect@timzander.com>
2019-06-04 11:39:13 -0500

    Complete 'About Amy' page

--- static/js/app.js
+++ static/js/app.js
@@ -2,7 +2,74 @@
 var ViewModel = function() {
   var self = this;
 
+  this.artNightText = ko.observable("Join me on Wednesday, June 26 at Sepia Chapel, Two Rivers, from 6-8:30pm for ALCOHOL INK WASHER JEWELRY!  Who doesn't like to repurpose things?  We have worked with alcohol inks before and this one is different because we are going small and on metal!  You will be creating seven washers, and a few littles for earrings too if you'd like.  It's fun to stack em for a different look.  You are the artists and as usual, color choices and designs are all up to you.  I will walk you through step by step!  Alcohol ink is one of the most unpredictable mediums to work with.  It is SO MUCH FUN!  I will teach you a few different techniques to get different results.  It can become addicting as the layers of ink pile on.  Just think, you will be able to wear your own artwork!  Wire, cording and ear wires will be available too so you will learn a few jewelry techniques as well.  No experience is necessary!  Enjoy a complimentary glass of wine as you create!  $35  Register below!");
 
+  this.artNightDate = ko.observable("Wednesday, June 26th 8:00PM - 9:30PM");
+
+  this.artNightLocation = ko.observable("Sepia Chapel 1820 Jefferson St Two Rivers, WI");
+
+  this.aboutAmyNavImg = ko.observable("../static/img/studio/about_me_thumbnail.png");
+
+  this.artNightNavImg = ko.observable("../static/img/studio/art_night_thumbnail.png");
+
+  this.shopNavImg = ko.observable("../static/img/studio/shop_thumbnail.png");
+
+  this.privateEventNavImg = ko.observable("../static/img/studio/private_event_thumbnail.png");
+
+  this.blogNavImg = ko.observable("../static/img/studio/blog_thumbnail.png");
+
+  this.contactNavImg = ko.observable("../static/img/studio/contact_thumbnail.png");
+
+  this.resetNavImgs = function() {
+    if (self.aboutAmyNavImg() != "../static/img/studio/about_me_thumbnail.png") {
+      self.aboutAmyNavImg("../static/img/studio/about_me_thumbnail.png");
+    }
+    else if (self.artNightNavImg() != "../static/img/studio/art_night_thumbnail.png") {
+      self.artNightNavImg("../static/img/studio/art_night_thumbnail.png");
+    }
+    else if (self.shopNavImg() != "../static/img/studio/shop_thumbnail.png") {
+      self.shopNavImg("../static/img/studio/shop_thumbnail.png");
+    }
+    else if (self.privateEventNavImg() != "../static/img/studio/private_event_thumbnail.png") {
+      self.privateEventNavImg("../static/img/studio/private_event_thumbnail.png");
+    }
+    else if (self.blogNavImg() != "../static/img/studio/blog_thumbnail.png") {
+      self.blogNavImg("../static/img/studio/blog_thumbnail.png");
+    }
+    else if (self.contactNavImg() != "../static/img/studio/contact_thumbnail.png") {
+      slef.contactNavImg("../static/img/studio/contact_thumbnail.png");
+    }
+  }
+
+  this.changeSrcAttrAboutAmy = function() {
+    self.resetNavImgs();
+    self.aboutAmyNavImg("../static/img/studio/about_me_thumbnail_selected.png");
+  }
+
+  this.changeSrcAttrArtNight = function() {
+    self.resetNavImgs();
+    self.artNightNavImg("../static/img/studio/art_night_thumbnail_selected.png");
+  }
+
+  this.changeSrcAttrShop = function() {
+    self.resetNavImgs();
+    self.shopNavImg("../static/img/studio/shop_thumbnail_selected.png");
+  }
+
+  this.changeSrcAttrPrivateEvent = function() {
+    self.resetNavImgs();
+    self.privateEventNavImg("../static/img/studio/private_event_thumbnail_selected.png");
+  }
+
+  this.changeSrcAttrBlog = function() {
+    self.resetNavImgs();
+    self.blogNavImg("../static/img/studio/blog_thumbnail_selected.png");
+  }
+
+  this.changeSrcAttrContact = function() {
+    self.resetNavImgs();
+    self.contactNavImg("../static/img/studio/contact_thumbnail_selected.png");
+  }
 }
 
 ko.applyBindings(new ViewModel());
--- static/js/model.js
+++ static/js/model.js
@@ -1,5 +1,19 @@
-var mainImages = [
-  "../static/img/fingers_love.jpg",
-  "../static/img/fingers_paint.jpg",
-  "../static/img/red_painting.jpg"
-]
+var aboutAmyText = "I'm a passionate artist with creativity that " +
+"needs to come out and be expressed daily.  My main focuses are working with clay, " +
+"fused glass, and painting, but love to dabble in new and creative methods " +
+"whenever I'm up for a fun challenge. Learning about art is as important to me " +
+"as teaching it, which I currently do at Art Night, as well as personal classes, " +
+"private events, and fundraisers. My inspiration comes from my environment and those " +
+"I love most, including my African Grey Parrot Louie! I find inspiration in most " +
+"things and always keep an open mind to new approaches and styles. I get to play " +
+"in the mud for a living and I can't imagine doing anything else!"
+
+var aboutAmyText2 = "I'm a passionate artist with creativity that " +
+"needs to come out and be expressed daily.  My main focuses are working with clay, " +
+"fused glass, and painting, but love to dabble in new and creative methods " +
+"whenever I'm up for a fun challenge. Learning about art is as important to me " +
+"as teaching it, which I currently do at Art Night, as well as personal classes, " +
+"private events, and fundraisers. My inspiration comes from my environment and those " +
+"I love most, including my African Grey Parrot Louie! I find inspiration in most " +
+"things and always keep an open mind to new approaches and styles. I get to play " +
+"in the mud for a living and I can't imagine doing anything else!"
